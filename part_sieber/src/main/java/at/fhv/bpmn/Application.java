a1dc2b482a9506a09d3f00a313cf382b66a23a05
Matthias Rupp <mru3383@students.fhv.at>
2021-01-19 18:25:18 +0100

    First attempt at simple process

--- part_sieber/src/main/java/at/fhv/bpmn/Application.java
+++ part_sieber/src/main/java/at/fhv/bpmn/Application.java
@@ -1,13 +1,38 @@
 package at.fhv.bpmn;
 
+import org.apache.catalina.webresources.TomcatURLStreamHandlerFactory;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
+@EnableProcessApplication
 public class Application {
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class);
-  }
+    public Application(ProcessEngine processEngine) {
+        this.processEngine = processEngine;
+    }
+
+    public static void main(String[] args) {
+        TomcatURLStreamHandlerFactory.disable();
+        SpringApplication.run(Application.class, args);
+    }
+
+    protected final ProcessEngine processEngine;
+
+    @PostConstruct
+    public void deployInvoice() {
+        ClassLoader classLoader = Application.class.getClassLoader();
+
+        if (processEngine.getIdentityService().createUserQuery().list().isEmpty()) {
+            processEngine.getRepositoryService()
+                    .createDeployment()
+                    .addInputStream("login.bpmn", classLoader.getResourceAsStream("login.bpmn"))
+                    .deploy();
+        }
+    }
 
 }
---
+++ part_sieber/src/main/java/at/fhv/bpmn/LoginWorker.java
@@ -0,0 +1,44 @@
+package at.fhv.bpmn;
+
+import org.camunda.bpm.client.ExternalTaskClient;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import java.util.HashMap;
+import java.util.logging.Logger;
+
+public class LoginWorker implements JavaDelegate {
+    private final static Logger LOGGER =Logger.getLogger(LoginWorker.class.getName());
+    private final static HashMap<String, String> userData = new HashMap<String, String>() {{
+        put("admin", "admin");
+        put("test", "abc");
+        put("mat", "1234");
+        put("jan", "22ik");
+    }};
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        ExternalTaskClient client = ExternalTaskClient.create()
+                .baseUrl("http://localhost:8080/engine-rest")
+                .asyncResponseTimeout(1000)
+                .build();
+
+        client.subscribe("check-login")
+                .lockDuration(1000)
+                .handler((externalTask, externalTaskService) -> {
+                    boolean correct = false;
+                    // put business logic
+
+                    String username = (String) externalTask.getVariable("username");
+                    String enteredPassword = (String) externalTask.getVariable("password");
+
+                    LOGGER.info("Attempting login for user " + username + "....");
+
+                    String correctPassword = userData.get(username);
+                    if(correctPassword != null) {
+                        if(correctPassword.equals(enteredPassword)) correct = true;
+                    }
+                    LOGGER.info("Login info correct: " + correct);
+                });
+    }
+}
